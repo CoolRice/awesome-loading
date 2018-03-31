c054d6c99b370848f39b5b7023c5b5b4057f52c3
coolrice <1251243609@qq.com>
2018-03-31 22:20:39 +0800

    webpack optimize
    1. add webpack-dev-server
    2. add css support

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.scss';
+ 
 
 class App extends React.Component {
   render(){
      return(
        <header>
-         <h1>AWESOME-LOADING</h1>
+         <h1>AWESOME-LOADING1</h1>
        </header>
       )
    }
 }
-  ReactDOM.render(<App />, document.querySelector('#app'))
+ReactDOM.render(<App />, document.querySelector('#app'))
--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,20 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'development',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  devtool: 'inline-source-map',
+    devServer: {
+    contentBase: './dist'
+  },
   module: {
     rules: [
       {
-        test: /\.js$/ ,
         loader:'babel-loader',
+        test: /\.js$/ ,
         exclude: '/node_modules/',
         options: {
           presets: [
@@ -18,7 +23,21 @@ module.exports = {
             ['env', {"targets": { "browsers": ['> 5%']}}]
           ]
         }
-      }
+      },
+      {
+            test: /\.scss$/,
+            use: [{
+                loader: "style-loader"
+            }, {
+                loader: "css-loader", options: {
+                    sourceMap: true
+                }
+            }, {
+                loader: "sass-loader", options: {
+                    sourceMap: true
+                }
+            }]
+        }
     ]
   }
 }
