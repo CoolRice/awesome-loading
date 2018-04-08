6621541490b962a54980056a4f2b67b9bc0b697b
yli <ying.li@magic-point.com>
2018-04-08 18:47:10 +0800

    optimize code

--- src/LoadingSection.js
+++ src/LoadingSection.js
@@ -1,45 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ThreeDotsScale from './LoadingItems/ThreeDotsScale';
-import ThreeDotsJump from './LoadingItems/ThreeDotsJump';
-import ThreeDotsRotate from './LoadingItems/ThreeDotsRotate';
-import ThreeDotsChase from './LoadingItems/ThreeDotsChase';
-import './index.scss';
 
 
 class LoadingSection extends React.Component {
+
+  renderItem(fragments) {
+    return fragments.map((fragment, index) =>
+      <div key={index} className="col"><div className="content">{fragment}</div></div>)
+  }
+
   render(){
     return (
       <section className="loading-section">
-         <div className="col">
-             <div className="content">
-               <ThreeDotsScale />
-             </div>
-         </div>
-         <div className="col">
-             <div className="content">
-               <ThreeDotsJump />
-             </div>
-         </div>
-         <div className="col">
-             <div className="content">
-               <ThreeDotsRotate />
-             </div>
-         </div>
-         <div className="col">
-             <div className="content">
-               <ThreeDotsChase />
-             </div>
-         </div>
-         <div className="col">
-             <div className="content">
-               <div className="loading-1">
-                 <div/>
-                 <div/>
-                 <div/>
-                 </div>
-             </div>
-         </div>
+         {
+           this.renderItem([
+             <div className="three-dots-scale"><div/><div/><div/></div>,
+             <div className="three-dots-chase"><div/><div/><div/></div>,
+             <div className="three-dots-rotate"><div/><div/><div/></div>,
+             <div className="three-dots-jump"><div/><div/><div/></div>,
+             <div className="three-dots-jump"><div/><div/><div/></div>,
+             <div className="three-dots-jump"><div/><div/><div/></div>,
+             <div className="three-dots-jump"><div/><div/><div/></div>,
+             <div className="three-dots-jump"><div/><div/><div/></div>,
+             <div className="three-dots-jump"><div/><div/><div/></div>,
+             <div className="three-dots-jump"><div/><div/><div/></div>,
+             <div className="three-dots-jump"><div/><div/><div/></div>,
+             <div className="three-dots-jump"><div/><div/><div/></div>,
+           ])
+         }
      </section>
     )
   }
